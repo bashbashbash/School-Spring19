982f892629a73077a3bff8323f9f984f5c2529d8
ZBASH <zlabas2@uic.edu>
2019-01-19 17:05:58 -0600

    Finished grabbing IP. Testing connection WIP

--- UIC/Spring19/CS450/hw1.c
+++ UIC/Spring19/CS450/hw1.c
@@ -2,12 +2,16 @@
 // Zack Labaschin
 // Reading and storing files was found at this url:
 // https://stackoverflow.com/questions/2029103/correct-way-to-read-a-text-file-into-a-buffer-in-c
-
+// socket setup taken from the following url:
+// https://www.geeksforgeeks.org/socket-programming-cc/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+
+#define PORT 25
 
 int debug = 1;
 
@@ -104,15 +108,57 @@ int main(int argc, char *argv[]) {
     tok = strtok(NULL, "\n");
     strcpy(mailClientSpaced, tok);
     strncpy(mailClient, &mailClientSpaced[1], 49);
-
-    if(debug) {printf("The result of the mailClient: %s", mailClient);}
+    if(debug) {printf("The result of the mailClient: %s\n", mailClient);}
   }
   pclose(mailhost);
 
-  int socketfd  = socket(AF_INET, SOCK_STREAM, 0);
-  struct sockaddr sockadd;
-  int addlength = sizeof(sockadd);
-  int connectfd = connect(socketfd, sockadd, )
+  char hostMailClinetCommand[1024] = {0};
+  strcat(hostMailClinetCommand, "host ");
+  strcat(hostMailClinetCommand, mailClient);
+  if(debug) {printf("This is the concat result of hostMailClinetCommand: %s\n", hostMailClinetCommand);}
+
+  FILE *mailIP = popen(hostMailClinetCommand, "r");
+  char IPSpaced[30];
+  char IP[30];
+
+  if(mailIP != NULL) {
+    fgets(popenText, 80, mailIP);
+    if(debug) {printf("The result of the popen: %s", popenText);}
+    tok = strtok(popenText, "0123456789");
+    tok = strtok(NULL, "\n");
+    strcpy(IPSpaced, tok);
+    strncpy(IP, &IPSpaced, 29);
+    if(debug) {printf("The result of the IP: %s\n", IP);}
+  }
+  pclose(mailIP);
+
+  // set the socket
+  int socketfd = -1;
+  if((socketfd = socket(AF_INET, SOCK_STREAM, 0) == -1)) {
+    printf("Rookie mistake - the socket is -1!\n");
+    return -1;
+  }
+
+  struct sockaddr_in servadd;
+  memset(&servadd, '0', sizeof(servadd));
+  servadd.sin_family = AF_INET;
+  servadd.sin_port   = htons(PORT);
+
+// converting addresses from text to binary
+  if(inet_pton(AF_INET, IP, &servadd.sin_addr) <= 0) {
+    printf("Rookie mistake - address invalid\n");
+    return -1;
+  }
+
+  if(connect(socketfd, (struct sockaddr *)&servadd, sizeof(servadd)) < 0) {
+    printf("\nRookie mistake - connection did not occur\n");
+    return -1;
+  }
+
+  send(socketfd, "helo server!", strlen("helo server!"), 0);
+  char receivedText[1024] = {0};
+  int retval = read(socketfd, receivedText, 1024);
+  printf("%s\n", receivedText);
 
   free(source);
   return 0;
